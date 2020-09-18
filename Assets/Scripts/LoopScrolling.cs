bdf2c82ca929791852715dcf6aa863a235a3b372
Aaron Mady <defiant00@gmail.com>
2020-09-17 20:07:32 -0400

    graphics

---
+++ Assets/Scripts/LoopScrolling.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class LoopScrolling : MonoBehaviour
+{
+	private float width, minX;
+
+	void Start()
+	{
+		width = GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+		minX = -(width + 8);
+	}
+
+	void Update()
+	{
+		while (transform.position.x < minX)
+		{
+			transform.position = new Vector3(transform.position.x + width, transform.position.y, transform.position.z);
+		}
+	}
+}
